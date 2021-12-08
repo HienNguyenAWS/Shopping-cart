7b8df18c08860b3ef0b8fb3c922d84c5cc8d36cd
Hien Nguyen <Hien.NguyenChat@nashtechglobal.com>
2021-12-08 11:31:23 +0700

    Initital project

---
+++ src/components/Card.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import CardBg from './CardBg';
+import CardInfo from './CardInfo';
+import ShoeData  from './ShoeData';
+
+const Card = ({updateCart}) => {
+
+	return (
+		<>
+			{ShoeData.map((shoe, index) => (
+				<div key={index} className="card">
+					<CardBg shoeImg = { shoe.cardImage } />
+					<CardInfo 
+						shoePrice = { shoe.cardPrice }
+						updateCart = {() => updateCart(shoe.cardImage, shoe.cardPrice)}
+					/>
+				</div>
+			))}
+		</>
+	)
+}
+
+export default Card;
---
+++ src/components/ShoeData.js
@@ -0,0 +1,34 @@
+import ShoeOne from '../img/shoe1.png';
+import ShoeTwo from '../img/shoe2.png';
+import ShoeThree from '../img/shoe3.png';
+import ShoeFour from '../img/shoe4.png';
+import ShoeFive from '../img/shoe5.png';
+import ShoeSix from '../img/shoe6.png';
+
+// eslint-disable-next-line import/no-anonymous-default-export
+export default [
+    {
+        cardImage: ShoeOne,
+        cardPrice: 100
+    },
+    {
+        cardImage: ShoeTwo,
+        cardPrice: 200
+    },
+    {
+        cardImage: ShoeThree,
+        cardPrice: 300
+    },
+    {
+        cardImage: ShoeFour,
+        cardPrice: 400
+    },
+    {
+        cardImage: ShoeFive,
+        cardPrice: 500
+    },
+    {
+        cardImage: ShoeSix,
+        cardPrice: 600
+    }
+];
